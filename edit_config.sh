4547a9b7cf2f2d33842fac339b52b87d9007e7c2
Ken L <klauer@bnl.gov>
2013-10-17 17:42:05 -0400

    use $EDITOR

--- edit_config.sh
+++ edit_config.sh
@@ -6,4 +6,9 @@ CONFIG_FILE=`ipython locate profile $PROFILE`/ipython_config.py
 echo "Profile is $PROFILE"
 echo "Configuration file location: $CONFIG_FILE"
 
-editor $CONFIG_FILE
+if [ -n "$EDITOR" ];
+then
+    $EDITOR $CONFIG_FILE
+else
+    editor $CONFIG_FILE
+fi
