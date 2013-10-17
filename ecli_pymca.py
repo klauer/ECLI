4969e9402e33ff1816cb3059ab94a764d78ae009
Ken L <klauer@bnl.gov>
2013-10-17 13:05:43 -0400

    pymca: update check working; multiprocessing option; SPS open on run

--- ecli_pymca.py
+++ ecli_pymca.py
@@ -16,6 +16,7 @@ import logging
 import multiprocessing as mp
 import xmlrpclib
 import threading
+import subprocess
 
 # IPython
 import IPython.utils.traitlets as traitlets
@@ -59,6 +60,7 @@ class ECLIpymca(ECLIPlugin):
 
     _callbacks = []
 
+    use_subprocess = traitlets.Bool(True, config=True)
     def __init__(self, shell, config):
         super(ECLIpymca, self).__init__(shell=shell, config=config)
         logger.debug('Initializing ECLI plugin ECLIpymca')
@@ -81,13 +83,35 @@ class ECLIpymca(ECLIPlugin):
         xmlrpc_plugin = get_plugin('ECLIxmlrpc')
         url = xmlrpc_plugin.server_url
 
-        self.pymca_process = mp.Process(target=start_pymca, args=(url, ))
-        self.pymca_process.start()
+        #print('pre launch', epics.caget('MLL:DXP:mca1.ERTM'))
+        #print('pid %d context %s' % (os.getpid(), epics.ca.current_context()))
+
+        try:
+            if self.use_subprocess:
+                args = [sys.executable, __file__, url]
+                self.pymca_process = subprocess.Popen(args)
+            else:
+                context = epics.ca.current_context()
+                epics.ca.detach_context()
+                self.pymca_process = mp.Process(target=start_pymca,
+                                                args=(url, ))
+                self.pymca_process.start()
+
+        except Exception as ex:
+            print("Failed to spawn pymca process (%s) %s" %
+                  (ex.__class__.__name__, ex))
+            self.pymca_process = None
+        finally:
+            if not self.use_subprocess:
+                epics.ca.attach_context(context)
 
     def exit(self):
         if self.pymca_process is not None:
-            print('Waiting for PyMca to close...')  # <-- TODO
-            self.pymca_process.join()
+            print('Waiting for PyMca to close...')
+            if hasattr(self.pymca_process, 'join'):
+                self.pymca_process.join()
+            #else:
+            #    self.pymca_process.wait()
 
 
 EPICS_TAG = 'EPICS'
@@ -100,11 +124,10 @@ EPICS_TAG = 'EPICS'
 SCAN_TAG = 'SCAN'
 SCAN_ENV_TAG = '%s_ENV' % (SCAN_TAG, )
 
-# TODO run updates in a separate thread
+
 class EpicsDetector(object):
     def __init__(self, name, pvs):
         print('--Detector %s--' % name)
-
         def update_array(arr, i, value=None, pvname=None, **kwargs):
             print('%s[%d]=%s (%s updated)' % (pvname, i, value, name))
             arr[i] = value
@@ -116,17 +139,20 @@ class EpicsDetector(object):
         for category, pvs_ in pvs.iteritems():
             arr = [0] * len(pvs_)
             setattr(self, category, arr)
+            #print('category', category, 'array', arr)
             for i, pv in enumerate(pvs_):
                 if not pv:
                     continue
 
                 cb = lambda arr=arr, i=i, **kwargs: update_array(arr, i, **kwargs)
-                print('Monitoring "%s"' % pv)
+                value = epics.caget(pv)
+                #print('Monitoring "%s" = %s (%s[%d])' % (pv, value, category, i))
                 epics.camonitor(pv, callback=cb)
-                cb(pvname=pv, value=epics.caget(pv))
-                print('Monitored "%s"' % pv)
+                cb(pvname=pv, value=value)
 
         # TODO assumes all spectra are same length
+        # sometimes type(self.spectrum[1]) == int? TODO: bug
+
         self.spectrum[0] = np.arange(len(self.spectrum[1]))
 
     @property
@@ -141,39 +167,30 @@ class EpicsDetector(object):
         for pv in self.pvs:
             epics.camonitor_clear(pv)
 
-#pymca_env = {'datafile': '/dev/null',
-#            'plotlist': 'list,of,plots,4,5',
-#            'title': 'title',
-#            'nopts': 5,
-#            #'axistitles': '1 2 3 4 5',
-#            'axistitles': 'a{1} b{2} c{3} d{4} e{5}',
-#            'H': 1.0,
-#            'K': 2.0,
-#            'L': 3.0,
-#            'xlabel': 'x label',
-#            'ylabel': 'y label',
-#            }
-
 
 class EpicsBridge(object):
     instance = None
     init_thread = None
 
-    # TODO pass the URL in with mp
-    def __init__(self, url):  #url='http://localhost:8988/epics_sps'):
+    def __init__(self, url):
         self.cache = {}
 
         print('Epics Bridge', url)
+        self._scan_info = {}
+        self._scan_data = {}
+        self._last_data = []
+
         self._url = None
+        # Setting the url property, causes the xmlrpc serverproxy to connect
         self.url = url
         self.detectors = {}
         self.array_list = [SCAN_TAG]
-        self.scan_arrays = []
+        self.scan_arrays = {}
 
         for detector, pv_info in self.server.get_detector_list():
             self.monitor_detector(detector, pv_info)
 
-        self.scan_info = {}
+        self._update_available = True
         self.update_timer = QTimer()
         self.update_timer.start(500)
         self.update_timer.timeout.connect(self.update)
@@ -188,7 +205,7 @@ class EpicsBridge(object):
     url = property(_get_url, _set_url, doc='XMLRPC server URL')
 
     def monitor_detector(self, name, pvs):
-        print('monitor detector', name, pvs)
+        print('Monitor detector %s (pvs: %s)' % (name, pvs))
         self.detectors[name] = det = EpicsDetector(name, pvs)
         self.array_list.append(name)
         if hasattr(det, 'calibration'):
@@ -201,6 +218,9 @@ class EpicsBridge(object):
         del self.detectors[name]
 
     def update(self):
+        if self.server is None:
+            return
+
         # check to add/remove detectors
         det_list = self.server.get_detector_list()
         det_dict = dict(det_list)
@@ -221,7 +241,15 @@ class EpicsBridge(object):
         return self.scan_info.get('nopts', 0), self.scan_info.get('_counters', 0)
 
     def _update_scan_info(self):
-        self.scan_info = info = self.server.get_scan_info()
+        if self.server is None or not self.update_available:
+            return
+
+        self._scan_info = info = self.server.get_scan_info()
+        if 'key' not in info:
+            print('scan info has no key?', info)
+            return
+
+        self._scan_key = info['key']
 
         ndim = info.get('_ndim', 1)
         labels = info.get('_labels', [])
@@ -233,12 +261,23 @@ class EpicsBridge(object):
         info['plotlist'] = label_str
         info['axistitles'] = label_str.replace(',', ' ')
 
-        del self.scan_arrays[:]
         if ndim == 2:
-            for label in labels:
-                self.scan_arrays.append('2D_%s' % label)
+            old_keys = set(self.scan_arrays.keys())
+            keys = set(['2D_%s' % label for label in labels])
+            if keys != old_keys:
+                remove_keys = old_keys - keys
+                for remove in remove_keys:
+                    del self.scan_arrays[remove]
+                for key in keys:
+                    if key not in self.scan_arrays:
+                        self.scan_arrays[key] = {'key': -1}
+        else:
+            self.scan_arrays.clear()
 
         print('got scan info', info)
+        self._last_data.append(self._scan_data)
+        self._scan_data = np.array(self.server.get_scan_data())
+        self._update_available = False
         return info
 
     def getarrayinfo(self, name):
@@ -260,28 +299,32 @@ class EpicsBridge(object):
             dim = self.scan_info.get('_dimensions')
             print('array info', name, dim)
             return [dim[0], dim[1], sps.FLOAT, sps.TAG_ARRAY | sps.TAG_IMAGE]
-        else:
-            print('unhandled array', name, self.scan_arrays)
 
     def getarraylist(self):
-        print('* array list is', self.array_list + self.scan_arrays)
-        return self.array_list + self.scan_arrays
+        return self.array_list + list(self.scan_arrays.keys())
 
-    def isupdated(self, name):
+    def _isupdated(self, name):
+        if self.server is None:
+            return False
+
         if name in self.detectors:
             if self.detectors[name].updated:
                 self.detectors[name].updated = False
                 return True
+
         elif name == SCAN_TAG:
-            key = self.server.get_scan_key()
-            print('server key', key, 'local key', self.scan_key)
-            return (key != self.scan_key)
+            return self.update_available
+
+        elif name in self.scan_arrays:
+            last_key = self.scan_arrays[name]['key']
+            return (self.update_available or last_key != self._scan_key)
 
         return False
 
-    @property
-    def scan_key(self):
-        return self.scan_info.get('scan_key', -1)
+    def isupdated(self, name):
+        ret = self._isupdated(name)
+        print('isupdated %s = %s' % (name, ret))
+        return ret
 
     def getenv(self, shmenv, key):
         print('getenv', shmenv, key)
@@ -290,28 +333,59 @@ class EpicsBridge(object):
         else:
             return ''
 
-    def getdata(self, name):
+    @property
+    def update_available(self):
+        if not self._update_available:
+            newest_key = self.server.get_scan_key()
+            self._update_available = (newest_key != self._scan_key)
+            return self._update_available
+        else:
+            return True
+
+    @property
+    def scan_info(self):
+        if self._scan_info is None or self.update_available:
+            self._update_scan_info()
+        return self._scan_info
+
+    @property
+    def scan_data(self):
+        if self._scan_data is None or self.update_available:
+            self._update_scan_info()
+        return self._scan_data
+
+    def _getdata(self, name):
         print('getdata', name)
         if name == SCAN_TAG:
-            return np.array(self.server.get_scan_data())
+            return self.scan_data
         elif name.endswith('_PARAM'):  # calibration
             detector = name.rsplit('_', 1)[0]
             return np.array([self.detectors[detector].calibration])
         elif name in self.detectors:
             return self.detectors[name].data.T  # expects transpose
         elif name in self.scan_arrays:
+            self.scan_arrays[name]['key'] = self._scan_key
+
+            # Strip off the 2D_ prefix
             name = name[3:]
             labels = self.scan_info['_labels']
             label_idx = labels.index(name)
 
-            data = np.array(self.server.get_scan_data())
+            data = self.scan_data
             data = data[:, label_idx]
-            print('data is', data, 'shape', np.shape(data))
-            print('dimensions', self.scan_info['_dimensions'])
+
+            if 0:
+                data = data + np.random.random(data.shape[0])
+            import traceback
+            traceback.print_stack()
             return np.array(data).reshape(self.scan_info['_dimensions'])
         else:
-            print('hmm? name=%s' % name)  # TODO
-            return [0]
+            return np.zeros(1)
+
+    def getdata(self, name):
+        ret = self._getdata(name)
+        print('getdata %s = %s' % (name, ret))
+        return ret
 
     def getkeylist(self, shmenv):
         #print('shmenv is', shmenv)
@@ -324,10 +398,8 @@ class EpicsBridge(object):
         return 1
 
     def getspeclist(self):
-        # TODO:
-        # Initializing the monitors prior to PyMca startup results in similar behavior
-        # as seen on the command line -- 4 second timeouts on arrays. Likely pyepics bug,
-        # but can't easily reproduce...
+        import traceback
+        traceback.print_stack()
         return [EPICS_TAG] + sps._getspeclist()
 
     # unused functions:
@@ -336,17 +408,16 @@ class EpicsBridge(object):
         return 0
 
     def getdatacol(self, shm,idx):
-        print('** unused? getdatacol idx', shm, idx)
-        return []
+        raise NotImplementedError
+        # unused in current PyMca code
 
     def getdatarow(self, shm,idx):
-        print('** unused? getdatarow idx', shm, idx)
-        return []
+        raise NotImplementedError
+        # unused in current PyMca code
 
     def putenv(self, shmenv,cmd,outp):
-        # unused in PyMca
-        print('epics putenv', shmenv, cmd, outp)
-        return 1
+        raise NotImplementedError
+        # unused in current PyMca code
 
 
 def wrap_sps(sps_fcn, epics_fcn):
@@ -365,12 +436,15 @@ def wrap_sps(sps_fcn, epics_fcn):
 
 def init_epicsbridge(xmlrpc_url):
     # TODO wrap this all up in the EpicsBridge class -- this isn't how it should be done
+    epics.ca.use_initial_context()
+
     while EpicsBridge.instance is None:
         try:
             EpicsBridge.instance = eb = EpicsBridge(xmlrpc_url)
         except Exception as ex:
             print('Unable to connect to EPICS bridge: (%s) %s' %
                   (ex.__class__.__name__, ex, ), file=sys.stderr)
+            raise
             time.sleep(5)
         else:
             for fcn in ('getarrayinfo', 'getarraylist', 'isupdated',
@@ -399,22 +473,41 @@ def wrap_pymca(xmlrpc_url):
         inst = EpicsBridge.instance
         inst.url = xmlrpc_url
 
-def pymca_print(s):
-    #print('(pymca) %s' % s, file=sys.__stdout__)
-    pass
+def pymca_print(s, end=''):
+    if 1:
+        with open('pymca_log.txt', 'at') as f:
+            print(s, end=end, file=f)
+    else:
+       print(s, end=end, file=sys.__stdout__)
 
 def start_pymca(xmlrpc_url):
     """
     .. note:: this runs in a separate process from ECLI
     """
 
-    wrap_pymca(xmlrpc_url)
+    if epics.__version__ <= '3.2.3':
+        epics._CACHE_.clear()
+        epics._MONITORS_.clear()
+        epics.ca._cache.clear()
+        epics.ca._put_done.clear()
+        epics.ca.libca = None
+        epics.ca.initial_context = None
+        reload(epics)
+        reload(epics.ca)
+
+        epics.ca.libca.ca_detach_context()
+        #print('pid %d context %s' % (os.getpid(), epics.ca.current_context()))
+        context = epics.ca.current_context()
+
+        #print('post launch', epics.caget('MLL:DXP:mca1.ERTM'))
+        #print('post launch m1', epics.caget('IOC:m1'))
 
     from ecli_util.misc import OutputStreamHandler
     sys.stdout = OutputStreamHandler(pymca_print)
     sys.stderr = OutputStreamHandler(pymca_print)
 
-    # TODO check instance
+    wrap_pymca(xmlrpc_url)
+
     from PyMca import PyMcaQt as qt
     app = qt.QApplication.instance()
     if app is None:
@@ -422,5 +515,14 @@ def start_pymca(xmlrpc_url):
 
     main = PyMca.PyMcaMain.PyMcaMain()
     main.show()
+
+    try:
+        source_sel = main.sourceWidget.sourceSelector
+        source_sel.openFile(EPICS_TAG, specsession=True)
+    except:
+        pass
+
     app.exec_()
 
+if __name__ == '__main__':
+    start_pymca(sys.argv[1])
