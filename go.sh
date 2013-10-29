a2740e8396347f89853c8a33573e5638ba5988e7
Ken L <klauer@bnl.gov>
2013-10-29 10:38:11 -0400

    IPython launch arguments

--- go.sh
+++ go.sh
@@ -5,7 +5,7 @@
 # ipython with the Qt console:
 #   ipython qtconsole --profile=ecli --pylab=inline
 
-PROFILE="${1:-ecli}"
+PROFILE="ecli"
 PROFILE_PATH=$(ipython locate profile $PROFILE)
 
 echo "Profile is located in: $PROFILE_PATH"
@@ -14,12 +14,14 @@ if [ -f "$PROFILE_PATH/ecli_config.py" ]
 then
     # If a configuration file exists in the profile directory, load it
     echo "Loading ECLI configuration from profile directory"
-    ipython --profile=$PROFILE -c "%load_config $PROFILE_PATH/ecli_config.py" -i
+    echo "IPython arguments: $@"
+    ipython --profile=$PROFILE -c "%load_config $PROFILE_PATH/ecli_config.py" -i $@
 elif [ -f ecli_config.py ]
 then
     # If a configuration file exists in the current directory, load it
     echo "Loading ECLI configuration from the current directory"
-    ipython --profile=$PROFILE -c "%load_config ecli_config.py" -i
+    echo "IPython arguments: $@"
+    ipython --profile=$PROFILE -c "%load_config ecli_config.py" -i $@
 else
-    ipython --profile=$PROFILE
+    ipython --profile=$PROFILE $@
 fi
